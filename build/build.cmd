7f44aab39c2265b44d20fb3f398b80c32797589a
Vaclav Turecek <vturecek@microsoft.com>
2017-04-19 11:39:38 -0700

    Updating nuget restore for vs2017 builds.

--- build/build.cmd
+++ build/build.cmd
@@ -2,7 +2,8 @@
 @if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild" set msbuildpath=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
 @if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild" set msbuildpath=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin
 
-dotnet restore %~dp0\..\src
+nuget.exe restore %~dp0\..\src\IoT.sln
+REM dotnet restore %~dp0\..\src
 
 "%msbuildpath%\msbuild" %~dp0\..\src\Iot.Admin.Application\Iot.Admin.Application.sfproj /t:Package
 "%msbuildpath%\msbuild" %~dp0\..\src\Iot.Ingestion.Application\Iot.Ingestion.Application.sfproj /t:Package
