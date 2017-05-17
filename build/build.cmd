2c521deb87bf9bc7c340448dbc67509614bc3258
Vaclav Turecek <vaclav@live.com>
2017-05-17 12:27:34 -0700

    Package updates.

--- build/build.cmd
+++ build/build.cmd
@@ -5,7 +5,7 @@
 nuget.exe restore %~dp0\..\src\IoT.sln
 REM dotnet restore %~dp0\..\src
 
-"%msbuildpath%\msbuild" %~dp0\..\src\Iot.Admin.Application\Iot.Admin.Application.sfproj /t:Package
-"%msbuildpath%\msbuild" %~dp0\..\src\Iot.Ingestion.Application\Iot.Ingestion.Application.sfproj /t:Package
-"%msbuildpath%\msbuild" %~dp0\..\src\Iot.Tenant.Application\Iot.Tenant.Application.sfproj /t:Package
-
+"%msbuildpath%\msbuild" %~dp0\..\src\Iot.Admin.Application\Iot.Admin.Application.sfproj /t:Package /p:Platform=x64
+"%msbuildpath%\msbuild" %~dp0\..\src\Iot.Ingestion.Application\Iot.Ingestion.Application.sfproj /t:Package /p:Platform=x64
+"%msbuildpath%\msbuild" %~dp0\..\src\Iot.Tenant.Application\Iot.Tenant.Application.sfproj /t:Package /p:Platform=x64
+ 
